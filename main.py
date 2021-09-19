e90296aa0bad0766fa8270b1fc1d62f79c98b962
Matvey <m9175712036@gmail.com>
2021-09-19 21:00:22 +0300

    stable with half visualisation

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ rule = int(input('enter num of rule: '))
 x = 4 * a + 3
 y = int((a - a % 2) * 1.5) + 1
 canvas = [[0 for j in range(x)] for i in range(y)]
-canvas2 = [[' ' for j in range(x)] for i in range(y)]
-canvas2[0][(x-1)//2] = '@'
+canvas2 = [['  ' for j in range(x)] for i in range(y)]
+canvas2[0][(x-1)//2] = '██'
 canvas[0][(x-1)//2] = 1
 
 arr = [0 for i in range(8)]
@@ -26,14 +26,14 @@ while pxn < x * y:
         current += canvas[pxn // x-1][pxn % x+1]
         canvas[pxn // x][pxn % x] = arr[current]
         if arr[current]:
-            canvas2[pxn // x][pxn % x] = '@'
+            canvas2[pxn // x][pxn % x] = '██'
         else:
-            canvas2[pxn // x][pxn % x] = ' '
+            canvas2[pxn // x][pxn % x] = '  '
         pxn += 1
         while pxn % x == 0 or pxn % x == x - 1:
              pxn+=1
         current = 0
 for i in canvas2:
     for j in range(1, x-1):
-        print(i[j], end = " ")
+        print(i[j], end = "")
     print("")
