90c8d27685a7c53fe61d0aeb2cc928b148c31728
Matvey <m9175712036@gmail.com>
2021-09-20 22:48:58 +0300

    stable optimised with progressbar

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from PIL import Image
 
 rows = int(input('enter num of rows: '))
@@ -13,8 +15,9 @@ def fractal(rows: int, rule: int):
 
     im = Image.new(mode="RGB", size=(x, y), color=COLOR_BACK)
 
-    canvas = [[0 for _ in range(x)] for _ in range(y)]
-    canvas2 = [['  ' for _ in range(x)] for _ in range(y)]
+    # canvas = [[0 for _ in range(x)] for _ in range(y)]
+    canvas = [[0 for _ in range(x)] for _ in range(2)]
+    # canvas2 = [['  ' for _ in range(x)] for _ in range(y)]
     arr = [0] * 8
 
     canvas[0][(x - 1) // 2] = 1
@@ -29,29 +32,29 @@ def fractal(rows: int, rule: int):
             arr[i] = 1
         else:
             arr[i] = 0
-
     arr = arr[::-1]
+
     current = 0
-    pxn = x + 1
+    u = 0
 
-    while pxn < x * y:
-        current += canvas[pxn // x - 1][pxn % x - 1] * 4
-        current += canvas[pxn // x - 1][pxn % x] * 2
-        current += canvas[pxn // x - 1][pxn % x + 1]
+    while u < (y - 1):
+        for i in range(1, x - 1):
+            current += canvas[0][i - 1] * 4
+            current += canvas[0][i] * 2
+            current += canvas[0][i + 1]
+            # print(i, current)
+            # print(canvas)
+            canvas[1][i] = arr[current]
 
-        canvas[pxn // x][pxn % x] = arr[current]
-
-        if arr[current]:
-            # canvas2[pxn // x][pxn % x] = '██'
-            im.putpixel((pxn % x, pxn // x), COLOR_MAIN)
-        else:
-            canvas2[pxn // x][pxn % x] = '  '
-
-        pxn += 1
-
-        while pxn % x == 0 or pxn % x == x - 1:
-            pxn += 1
-        current = 0
+            if arr[current]:
+                # canvas2[i // x][i % x] = '██'
+                im.putpixel((i, u + 1), COLOR_MAIN)
+            current = 0
+        # else:
+        # canvas2[i // x][i % x] = '  '
+        canvas[0] = [j for j in canvas[1]]
+        u += 1
+        print(f'\r[{u + 1}/{y} ({((u + 1) / y * 100):.5f} %)', end='')
     '''
     for i in canvas2:
         for j in range(1, x - 1):
@@ -59,6 +62,7 @@ def fractal(rows: int, rule: int):
         print("")
     '''
     im.show()
+    im.save(f'C:/Users/matve/Desktop/fractals/{str(randint(1000000, 9999999))}.png')
 
 
 fractal(rows, rule)
