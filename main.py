76c81dd8d1d9e0cc749e14cd36a46d32185052c5
PM-95025 <50608884+pm-95025@users.noreply.github.com>
2022-02-15 16:28:21 +0300

    reindented with tabs, added tqdm progressbar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from random import randint
 from PIL import Image
+from tqdm import tqdm
 
 rows = int(input('enter num of rows: '))
 rule = int(input('enter num of rule: '))
@@ -9,51 +10,43 @@ COLOR_MAIN = (0, 0, 0)
 
 
 def fractal(rows: int, rule: int):
-    print('[...] loading...', end='')
+	rule_ = rule
+	print('[...] loading...', end='')
 
-    x = 2 * rows + 1
-    y = rows
-    im = Image.new(mode="RGB", size=(x, y), color=COLOR_BACK)
-    # canvas = [[0 for _ in range(x)] for _ in range(y)]
-    canvas = [[0 for _ in range(x)] for _ in range(2)]
-    # canvas2 = [['  ' for _ in range(x)] for _ in range(y)]
-    canvas[0][(x - 1) // 2] = 1
-    arr = [0] * 8
-    current = 0
-    # canvas2[0][(x - 1) // 2] = '██'
-    im.putpixel(((x - 1) // 2, 0), COLOR_MAIN)
+	x = 2 * rows + 1
+	y = rows
+	im = Image.new(mode="RGB", size=(x, y), color=COLOR_BACK)
+	canvas = [[0 for _ in range(x)] for _ in range(2)]
+	canvas[0][(x - 1) // 2] = 1
+	arr = [0] * 8
+	current = 0
+	im.putpixel(((x - 1) // 2, 0), COLOR_MAIN)
 
-    for i in range(7, 0, -1):
-        if rule >= 2 ** i:
-            rule -= 2 ** i
-            arr[i] = 1
-        else:
-            arr[i] = 0
-    arr = arr[::-1]
+	for i in range(7, 0, -1):
+		if rule >= 2 ** i:
+			rule -= 2 ** i
+			arr[i] = 1
+		else:
+			arr[i] = 0
+	arr = arr[::-1]
 
-    print('', end='\r')
+	print('', end='\r')
 
-    for u in range(y - 1):
-        for i in range(1, x - 1):
-            current += canvas[0][i - 1] * 4
-            current += canvas[0][i] * 2
-            current += canvas[0][i + 1]
-            canvas[1][i] = arr[current]
+	for u in tqdm(range(y - 1)):
+		for i in range(1, x - 1):
+			current += canvas[0][i - 1] * 4
+			current += canvas[0][i] * 2
+			current += canvas[0][i + 1]
+			canvas[1][i] = arr[current]
 
-            if arr[current]:
-                # canvas2[i // x][i % x] = '██'
-                im.putpixel((i, u + 1), COLOR_MAIN)
-            current = 0
-        canvas[0] = [j for j in canvas[1]]
-        print(f'\r[{u + 2}/{y} ({((u + 2) / y * 100):.5f} %)', end='')
-    '''
-    for i in canvas2:
-        for j in range(1, x - 1):
-            print(i[j], end="")
-        print("")
-    '''
-    im.show()
-    im.save(f'C:/Users/matve/Desktop/fractals/{str(randint(1000000, 9999999))}.png')
+			if arr[current]:
+				im.putpixel((i, u + 1), COLOR_MAIN)
+			current = 0
+		canvas[0] = [j for j in canvas[1]]
+
+
+	im.show()
+	im.save(f'./{str(randint(1000000, 9999999))}-{rule_}.png')
 
 
 fractal(rows, rule)
